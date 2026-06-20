044173b31970b9798332b489d49871f5131a59a1
Fauziyah Martha Aula <FauziyahMarthaAula@saintek.unipdu.ac.id>
2026-06-20 18:17:35 +0700

    coba

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    //
+    protected $table = 'produks';
+
+    protected $fillable = [
+        'nama',
+        'deskripsi',
+        'harga',
+        'gambar',
+    ];
 }
